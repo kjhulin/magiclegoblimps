abb15811828ff02995aa1b65d8654b21ce447cad
JoeGebhard <JoeGebhard@445d4ad4-0937-11df-b996-818f58f34e26>
2010-05-04 17:47:50 +0000

    [Server] instalkll script modification

--- Server/Controller/install.sh
+++ Server/Controller/install.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+arch=`uname -r |grep 64`
+echo "Installing libraries to /usr/local/lib"
+if [ "$arch." = "64." ]
+then
+    cp -f lib/64/*.so* /usr/local/lib/
+    if [ $? -ne 0 ]
+    then
+        echo "Library install failed.  Are you root?"
+	exit 1
+    fi
+else
+    cp -f lib/32/*.so* /usr/local/lib/
+    if [ $? -ne 0 ]
+    then
+        echo "Library install failed.  Are you root?"
+	exit 1
+    fi
+fi
+
 
 echo "Building MLBServer..."
 make
@@ -9,33 +28,14 @@ then
     exit 1
 fi
 
-echo "Installing libraries to /usr/local/lib"
-arch = `uname -r |grep 64`
-if [ "$arch." = "64." ]
-then
-    cp -f lib/*64.so /usr/local/lib/
-    if [ $? -ne 0 ]
-    then
-        echo "Library install failed.  Are you root?"
-	exit 1
-    fi
-else
-    cp -f lib/*32.so /usr/local/lib/
-    if [ $? -ne 0 ]
-    then
-        echo "Library install failed.  Are you root?"
-	exit 1
-    fi
-fi
-
 echo "Installing MLBServer binary to /usr/local/bin"
-cp -f build/ServerController /usr/local/bin/MLBServer
+cp -f build/ServerController /usr/local/bin/MLBServer.bin
+cp -f MLBServer /usr/local/bin/MLBServer
 if [ $? -ne 0 ]
 then
     echo "Install failed. Are you root?"
     exit 1
 fi
-
 
 echo "Done!"
 exit 0
